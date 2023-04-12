917080172ca102365e1e7040ef48f94d3786b0af
xkovac52 <davidkovacspse@gmail.com>
2023-04-12 09:39:44 +0200

    Improved input.vhdl

--- labs/Project/project_morse/project_morse.srcs/sources_1/new/input.vhd
+++ labs/Project/project_morse/project_morse.srcs/sources_1/new/input.vhd
@@ -44,115 +44,34 @@ end input;
 
 architecture Behavioral of input is
 
-signal sig_let  : String(0 to 4) := "UUUUU";
+signal sig_let  : std_logic_vector(9 downto 0);
 signal c        : integer := 0;
-signal toBits   : std_logic_vector(9 downto 0);
 
 begin
     P_reader : process is
     begin
     toBits(9)<= '0'; --Zjednoduši? a odstarni? case when
     toBits(8)<= '1';
+		if(c > 4) then
+			c = 0;
+			sig_let <= "0000000000";
+		end if;
         if(dot = '1') then
-        sig_let(c) <= 'T';
-        c<= c+1;
+			sig_let(9-c) <= '0';
+			sig_let(9-c-1) <= '1';
+			c<= c+2;
         end if;
         if(dash = '1') then
-        sig_let(c) <= 'C';
-        c<= c+1;
+			sig_let(9-c) <= '1';
+			sig_let(9-c-1) <= '1';
+			c<= c+2;
         end if;
         if(rst = '1') then
-        sig_let <= "UUUUU";
-        c<= 0;
+			sig_let <= "0000000000";
+			c<= 0;
         end if;
         if(send = '1') then
-        c<= 0;
+			letter <= sig_let;
         end if;    
   end process P_reader;
-  P_convertor : process is
-    begin
-       case sig_let is 
-            when "TCUUU" => --A
-            toBits <= "0111000000";
-            when "CTTTU" => --B
-            toBits <= "0111000000";
-            when "CTCTU" => --C
-            toBits <= "0111000000";
-            when "CTT" => --D
-            toBits <= "0111000000";
-            --------------------------
-            when "TUUUU" => --E
-            toBits <= "0111000000";
-            when "TTCTU" => --F
-            toBits <= "0111000000";
-            when "CCTUU" => --G
-            toBits <= "0111000000";
-            when "TTTTU" => --H
-            toBits <= "0111000000";
-            --------------------------
-            when "TCUUU" => --I
-            toBits <= "0111000000";
-            when "TCUUU" => --J
-            toBits <= "0111000000";
-            when "TCUUU" => --K
-            toBits <= "0111000000";
-            when "TCUUU" => --L
-            toBits <= "0111000000";
-            --------------------------
-            when "TCUUU" => --M
-            toBits <= "0111000000";
-            when "TCUUU" => --N
-            toBits <= "0111000000";
-            when "TCUUU" => --O
-            toBits <= "0111000000";
-            when "TCUUU" => --P
-            toBits <= "0111000000";
-            --------------------------
-            when "TCUUU" => --Q
-            toBits <= "0111000000";
-            when "TCUUU" => --R
-            toBits <= "0111000000";
-            when "TCUUU" => --S
-            toBits <= "0111000000";
-            when "TCUUU" => --T
-            toBits <= "0111000000";
-           --------------------------
-            when "TCUUU" => --U
-            toBits <= "0111000000";
-            when "TCUUU" => --V
-            toBits <= "0111000000";
-            when "TCUUU" => --W
-            toBits <= "0111000000";
-            when "TCUUU" => --X
-            toBits <= "0111000000";
-            --------------------------
-            when "TCCCC" => --Y
-            toBits <= "1111111111"; 
-            when "TTCCC" => --Z
-            toBits <= "1111111111"; 
-            when "TCCCC" => --1
-            toBits <= "1111111111"; 
-            when "TTCCC" => --2
-            toBits <= "1111111111"; 
-            --------------------------
-            when "TTTCC" => --3
-            toBits <= "1111111111"; 
-            when "TTTTC" => --4
-            toBits <= "1111111111"; 
-            when "TTTTT" => --5
-            toBits <= "1111111111"; 
-            when "CTTTT" => --6
-            --------------------------
-            toBits <= "1111111111"; 
-            when "CCTTT" => --7
-            toBits <= "1111111111"; 
-            when "CCCTT" => --8
-            toBits <= "1111111111"; 
-            when "CCCCT" => --9
-            toBits <= "1111111111";  
-            when others => --0
-            toBits <= "1111111111"; 
-            --------------------------
-        end case;    
-  end process P_convertor;
 end Behavioral;
